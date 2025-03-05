a6019fc51600eaaa49b37ab5ec6a7edff0a286de
muskan-015 <muskanvds2004@gmail.com>
2025-03-05 22:24:57 +0530

    [Muskan]Add. UC5 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'] ,
+  standalone: true,
+  imports: [FormsModule] 
 })
 export class AppComponent {
   message: string = "Hello from BridgeLabz";  
-  logoPath: string = "E:/Capg/Angular/my-angular-app/public/BL_logo_square_jpg.jpg";   
+  logoPath: string = "D:/Angular/Angular_App/assets/BL_logo_square_jpg.jpg";   
+  userName: string = "";
 
   openBridgeLabz() {
     window.open("https://www.bridgelabz.com", "_blank");
